e12fcac56819a98aede8f9675eaea1213e47fd3b
naxer-12 <rexan.2212@gmail.com>
2021-06-13 15:02:20 +0530

    Added USER DESIGN for add order

---
+++ FoodGrid/app/src/main/java/com/example/foodgrid/UserOrder.java
@@ -0,0 +1,14 @@
+package com.example.foodgrid;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class UserOrder extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user_order);
+    }
+}
