0887aac8253b22167732ad70a3e1a0f57131dd2f
maitrimodi <maitrimodi19@gmail.com>
2021-06-14 13:36:42 +0530

    Conflict solved

--- FoodGrid/app/src/main/java/com/example/foodgrid/RegisterActivity.java
+++ FoodGrid/app/src/main/java/com/example/foodgrid/RegisterActivity.java
@@ -4,17 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-import com.example.foodgrid.databinding.ActivityUserOrderBinding;
-
-public class MainActivity extends AppCompatActivity {
-
-
-
+public class RegisterActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_register);
     }
 }
