f3f2c26e274b21328b75bf910d0e3e56b48ec2db
naxer-12 <rexan.2212@gmail.com>
2021-06-15 22:21:23 +0530

    fixed merge conflicts and errors in develop

--- FoodGrid/app/src/main/java/com/example/foodgrid/ViewProfileActivity.java
+++ FoodGrid/app/src/main/java/com/example/foodgrid/ViewProfileActivity.java
@@ -8,6 +8,7 @@ import android.view.View;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.example.foodgrid.dao.UserDAO;
 import com.example.foodgrid.databinding.ActivityViewProfileBinding;
 import com.example.foodgrid.model.User;
 
